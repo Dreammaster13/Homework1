84f419438b2d1177b36b58cfa9beafcb488bc1c7
Rim Fardiyev <threeacesstl@gmail.com>
2018-10-31 01:17:51 -0400

    Homework1 complete. (with bonus missions)

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -6,7 +6,23 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Please enter numbers and press Return:");
+            
+            string userInput = Console.ReadLine();
+            string message = "";
+
+            if (userInput == "4 8 15 16 23 42")
+                message = "Recalibrating energy core ... Complete!";
+            else
+                Console.Beep(50, 500);
+                
+                message = "FAILURE!";
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
     }
 }
